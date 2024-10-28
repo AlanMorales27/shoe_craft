6f1cdf2551a023afbff2e9de49a2d168f32b53e4
AlanMorales27 <alanmoralesg47@gmail.com>
2024-10-28 07:23:46 -0500

    Add preview model and config lights

--- scripts/three.js
+++ scripts/three.js
@@ -1,19 +1,31 @@
 import * as THREE from 'three';
 // Import addons
 import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls';
+import  { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader';
 
 //Scene config
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const renderer = new THREE.WebGLRenderer({alpha: true});
+const renderer = new THREE.WebGLRenderer
+({
+	alpha: true,
+	antialias: true,
+});
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0xfff } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// Light imports
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+const light_1 = new THREE.DirectionalLight(0xffffff, 1);
+const light_2 = new THREE.DirectionalLight(0xffffff, 1);
+light_1.position.set(0, 5, 5);
+light_2.position.set(0,5,-5);
+scene.add(
+	light_1,
+	light_2, 
+	ambientLight
+);
 
 // Orbit Control Settings
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -21,16 +33,22 @@ controls.enableZoom = false; //Don't enable zoom
 controls.enablePan = false; //Don't enable camera axis move
 controls.enableDamping = true; //Give smoothing 
     controls.dampingFactor = 0.05;
-controls.maxDistance = 3;
+controls.maxDistance = 2;
 camera.position.z = 5;
+
+// Loader
+const loader = new GLTFLoader();
+loader.load(
+	'../source/models/Trainer.glb',
+	function(gltf){
+		scene.add(gltf.scene);
+		gltf.scene.scale.set(.2,.2,.2);
+	}
+)
 
 function animate() {
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
     controls.update();
-
 	renderer.render( scene, camera );
 
 }
